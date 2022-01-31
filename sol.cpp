59d14e5af1e6d0f35d2cd46b75c9f69163567916
Advitiya <advitiyajadhav1@gmail.com>
2022-01-30 23:51:50 -0500

    updated template

--- sol.cpp
+++ sol.cpp
@@ -3,16 +3,12 @@
 using namespace std;
 const int MN=1e5+10;
 void setIO(string str){
-	if((int)str.size()==0){
-		ios_base::sync_with_stdio(false);cin.tie(nullptr);
-	}
-	else{
-		ios_base::sync_with_stdio(false);cin.tie(nullptr);
-		freopen((str+".in").c_str(),"r",stdin);
-		freopen((str+".out").c_str(),"w",stdout);
-	}
+	freopen((str+".in").c_str(),"r",stdin);
+	freopen((str+".out").c_str(),"w",stdout);
 }
 int main(){
-    
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+	//setIO("");
+	cout << "Hello World" << "\n";
     return 0;
 }
