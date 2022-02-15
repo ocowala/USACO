c95169fe06e44111096b51abf9d868e82061cd13
Advitiya Jadhav <advitiyajadhav1@gmail.com>
2022-02-14 22:03:32 -0500

    using nvim

--- sol.cpp
+++ sol.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 #define dbg(...) cerr << #__VA_ARGS__ << ":" << __VA_ARGS__ << "\n"
-#define ll long long
 using namespace std;
 const int MOD=1000000007,MAX_N=2e5;
 void setIO(string str){freopen((str+".in").c_str(),"r",stdin);freopen((str+".out").c_str(),"w",stdout);}
 int main(){
-    //setIO("");
-    ios_base::sync_with_stdio(false),cin.tie(nullptr),cin.exceptions(ios_base::failbit);
-    
-    return 0;  
+	//setIO("");
+	ios_base::sync_with_stdio(false),cin.tie(nullptr),cin.exceptions(ios_base::failbit);
+	cout << "Hello World" << "\n";
+	return 0;
 }
