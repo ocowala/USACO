c16829b76f52d4f98ad9059296905da498e29287
ocowala <advitiyajadhav1@gmail.com>
2022-03-22 16:32:15 -0400

    new template for sublime trying to switch to it for cpp

--- a.cpp
+++ a.cpp
@@ -1,7 +1,27 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
+void setIO(string str){
+	if((int) str.size() > 0) freopen((str+".in").c_str(),"r",stdin),freopen((str+".out").c_str(),"w",stdout);
+	cin.tie(nullptr);ios_base::sync_with_stdio(false);cin.exceptions(ios_base::failbit);
+}
 int main(){
-	cin.tie(0)->sync_with_stdio(0);
+	setIO("");
 	
 	return 0;
 }
