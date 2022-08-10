7a4b9be2f3676c6def51329daba3d08a0994099e
ocowala <advitiyajadhav1@gmail.com>
2022-08-10 17:44:48 -0400

    last commit

--- a.cpp
+++ a.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve(){
+    
+}
 int main(){
     cin.tie(0)->sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
     return 0;
 }
--- learning/template.cpp
+++ learning/template.cpp
@@ -1,13 +1,10 @@
 #include <bits/stdc++.h>
-//#include <stdlib.h>
 using namespace std;
 int main(){
 	//freopen("a.in","r",stdin);
 	//freopen("a.out","w",stdout);
 	cin.tie(0)->sync_with_stdio(0);
-	cout << "Hello World" << "\n";		
-	cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-
-
+	cout << "Hello World\n";
+	cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
 	return 0;
 }
